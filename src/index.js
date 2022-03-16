070d6c1e370c275c887c0362ce64dc8e2ec6190c
Albin Frick <abbzez@gmail.com>
2022-03-17 00:02:31 +0100

    Refactor functions

--- src/index.js
+++ src/index.js
@@ -17,69 +17,63 @@ form.addEventListener('submit', (e) => {
 })
 
 
-const imgError = () => {
-	console.log('error loading image')
+
+
+const toggleImg = (imgContainer) => {
+	imgContainer.classList.contains('big') ? imgContainer.classList.remove('big') : imgContainer.classList.add('big')
+}
+
+var lazyloadThrottleTimeout;
+const lazyload = () => {
+	var lazyloadImages = document.getElementsByClassName('lazy')
+	lazyloadImages = Array.prototype.slice.call(lazyloadImages)
+	if (lazyloadThrottleTimeout) {
+		clearTimeout(lazyloadThrottleTimeout);
+	}
+	lazyloadThrottleTimeout = setTimeout(function () {
+		var scrollTop = window.pageYOffset;
+		lazyloadImages.forEach(function (img) {
+			if (img.offsetTop < (window.innerHeight + scrollTop)) {
+				img.src = img.dataset.src;
+				img.classList.remove('lazy');
+			}
+		});
+		if (lazyloadImages.length == 0) {
+			page = page + 1
+			fetchImages(page, search)
+			document.removeEventListener("scroll", lazyload);
+			window.removeEventListener("resize", lazyload);
+		}
+	}, 20);
+}
+
+const buildImage = (photo) => {
+	let imgContainer = document.createElement('figure');
+	imgContainer.classList.add('imgContainer');
+	let img = document.createElement('img');
+	img.setAttribute('data-src', photo.imgURI + '_m.jpg');
+	img.setAttribute('alt', photo.title);
+	img.classList.add('lazy')
+	img.classList.add('loader')
+	imgContainer.addEventListener('click', () => toggleImg(imgContainer))
+	let title = document.createElement('figcaption');
+	title.setAttribute('class', 'titleContainer')
+	const titleText = document.createTextNode(photo.title ? photo.title : 'Ingen titel hittades');
+	title.appendChild(titleText);
+	imgContainer.appendChild(img);
+	imgContainer.appendChild(title);
+	gallery.appendChild(imgContainer);
 }
 
 const fetchImages = (page, search) => {
 	fetch(`http://localhost:8080/search-image/${search}/30/${page}`).then((res) => {
 		return res.json()
 	}).then((response) => {
-		console.log(response)
-		console.log(page)
-		page++
 		document.getElementById('loading').setAttribute('class', 'hide');
-
 		response.map((photo) => {
-			let imgContainer = document.createElement('div');
-			imgContainer.classList.add('imgContainer');
-			let img = document.createElement('img');
-			img.setAttribute('data-src', photo.imgURI);
-			img.setAttribute('alt', photo.title);
-			img.classList.add('lazy')
-			img.classList.add('loader')
-			let titleContainer = document.createElement('div');
-			titleContainer.setAttribute('class', 'titleContainer')
-			let title = document.createElement('p');
-			const titleText = document.createTextNode(photo.title);
-			title.appendChild(titleText);
-			titleContainer.appendChild(title);
-			imgContainer.appendChild(img);
-			imgContainer.appendChild(titleContainer);
-			gallery.appendChild(imgContainer);
+			buildImage(photo)
 		})
-
-
-		// var lazyloadImages = document.getElementsByClassName('lazy')
-		// lazyloadImages = Array.prototype.slice.call(lazyloadImages)
-		// console.log(lazyloadImages)
-
-		var lazyloadThrottleTimeout;
-
-
-		function lazyload() {
-			var lazyloadImages = document.getElementsByClassName('lazy')
-			lazyloadImages = Array.prototype.slice.call(lazyloadImages)
-			if (lazyloadThrottleTimeout) {
-				clearTimeout(lazyloadThrottleTimeout);
-			}
-			lazyloadThrottleTimeout = setTimeout(function () {
-				var scrollTop = window.pageYOffset;
-				lazyloadImages.forEach(function (img) {
-					if (img.offsetTop < (window.innerHeight + scrollTop)) {
-						img.src = img.dataset.src;
-						img.classList.remove('lazy');
-					}
-				});
-				if (lazyloadImages.length == 0) {
-					fetchImages(page, search)
-					document.removeEventListener("scroll", lazyload);
-					window.removeEventListener("resize", lazyload);
-				}
-			}, 20);
-		}
 		lazyload()
-
 		document.addEventListener("scroll", lazyload);
 		window.addEventListener("resize", lazyload);
 
@@ -89,6 +83,5 @@ const fetchImages = (page, search) => {
 		const errText = document.createTextNode('Kunde tyvärr inte hämta några bilder. Prova att ladda om din webbläsare.');
 		err.appendChild(errText)
 		gallery.appendChild(err)
-
 	})
 }
